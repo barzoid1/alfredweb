e830e6aa9095c55a09dfbae64704e9ee6f56e67c
barzoid1 <128053888+barzoid1@users.noreply.github.com>
2023-06-22 02:24:53 +0100

    Update script.js

--- alfred/js/script.js
+++ alfred/js/script.js
@@ -1,7 +1,7 @@
 menu.onclick = () => {
     menu.classList.toggle('bx-x')
 }
-window.addEventListener("load", function() {
+window.onLoad = function() {
     var loader = document.querySelector('#loader')
     loader.parentNode.removeChild(loader)
   })
