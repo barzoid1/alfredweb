96990469d89614cf503b620742e3cf0a40a9fd3d
barzoid1 <128053888+barzoid1@users.noreply.github.com>
2023-06-24 16:47:57 +0100

    navbarissue

--- alfred/js/script.js
+++ alfred/js/script.js
@@ -1,7 +1,15 @@
-menu.onclick = () => {
-    menu.classList.toggle('bx-x')
-}
 window.addEventListener("load", function() {
   const loader = document.querySelector('#loader')
   this.document.body.removeChild(loader)
 })
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  var myOffcanvas = document.getElementById('offcanvasDarkNavbar');
+  var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+  document.getElementById("OpenMenu").addEventListener('click', function (e){
+    e.preventDefault();
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  });
+});
