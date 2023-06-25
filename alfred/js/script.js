69362e83b0a7d22a053c4d043bdd561085d830cb
barzoid1 <128053888+barzoid1@users.noreply.github.com>
2023-06-25 12:52:08 +0100

    bottom to top bt

--- alfred/js/script.js
+++ alfred/js/script.js
@@ -21,7 +21,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
